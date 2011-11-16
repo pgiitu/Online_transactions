023334901fa18dafde5aad2c2e69a14fc5185883
sonu giri <sonu.iitp@gmail.com>
2011-11-16 22:29:34 +0530

    test added

--- transactions/tests.py
+++ transactions/tests.py
@@ -7,6 +7,18 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
+from transactions.models import Transaction
+from transactions.models import Connected_Account
+from transactions.models import Connected_Account_Interbank
+from transactions.models import Bank
+from transactions.models import State
+from transactions.models import Branch
+from transactions.models import Account
+from transactions.models import Bank_Account
+from decimal import Decimal
+import datetime
+import unicodedata
+
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
