3eae169d2d2a9f6810c349b7a6806255764ee62b
chhavi mittal <chhavi.mittal91@gmail.com>
2011-11-17 04:23:16 -0800

    latest changes in the code

--- transactions/models.py
+++ transactions/models.py
@@ -1,6 +1,14 @@
+"""
+It contains the models for the online_transaction modeule 
+"""
+
 from django.db import models
 
 class Account(models.Model):
+  """
+  It contains the username and password of the user and some other details liel his mobile number.
+  It also contains a unique user_id
+  """
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=200)
     mobile_no = models.CharField(max_length=10)
@@ -9,6 +17,11 @@ class Account(models.Model):
         return self.username
 
 class Transaction(models.Model):
+  """
+  This class contains the details of the transaction done by the user.
+  It contains details like account_number from which transaction is done, receiver's account number, transaction status, transaction type
+  and other related information.
+  """
     t_amount=models.CharField(max_length=10)
     t_sender_acc_no = models.CharField(max_length=10)
     t_receiver_acc_no = models.CharField(max_length=10)
@@ -21,6 +34,10 @@ class Transaction(models.Model):
         return self.t_sender_acc_no
 
 class Connected_Account(models.Model):
+  """
+  This class stores the account details and other information of the receiver's account 
+  so that the user doesn't has to fill in all teh details of the receiver again and again
+  """
     ca_host_acc_id = models.CharField(max_length=10)
     ca_name=models.CharField(max_length=25)
     ca_connected_acc_no = models.CharField(max_length=10)
@@ -31,6 +48,12 @@ class Connected_Account(models.Model):
         return self.ca_host_acc_id
 
 class Connected_Account_Interbank(models.Model):
+  """
+  this class stores the account details and other information of the receiver's account 
+  so that the user doesn't has to fill in all teh details of the receiver again and again.
+  This class stores details of user for interbank transfer as more information is required for 
+  inetrbank compared to funds transfer
+  """
     ca_host_acc_no = models.CharField(max_length=10)
     ca_acc_no = models.CharField(max_length=10)
     ca_addition_date = models.DateTimeField('Date of Addition')
@@ -44,11 +67,17 @@ class Connected_Account_Interbank(models.Model):
         return self.ca_host_acc_no
 
 class Bank(models.Model):
+  """
+  this class stores the names of all tha banks
+  """
     bank_name = models.CharField(max_length=10)
     def __unicode__(self):
         return self.bank_name
 
 class State(models.Model):			# i have used the state instead of State as proposed by us in the class diagram
+    """
+    this class stores the name of all the states and the banks which each state has
+    """
     bank = models.ForeignKey(Bank)
     state_name = models.CharField(max_length=20)
     def __unicode__(self):
@@ -56,6 +85,9 @@ class State(models.Model):			# i have used the state instead of State as propose
 
 
 class Branch(models.Model):
+  """
+  this class stores each branch and its ifsc code and it has a foreign key to Bank and State class.
+  """
     branch = models.ForeignKey(State)
     branch_name = models.CharField(max_length=10)
     ifsc_code = models.CharField(max_length=10)  # i have put it a char if needed we can change it to integer
@@ -63,6 +95,10 @@ class Branch(models.Model):
         return self.branch_name
 
 class Bank_Account(models.Model):
+  """
+  This class stores the bank account number, user_id, his account type, and other relevant information 
+  of a user.
+  """
     ba_user_id = models.ForeignKey(Account)
     ba_acc_no = models.CharField(max_length=15)
     ba_ifsc_code = models.CharField(max_length=10)  # i have put it a char if needed we can change it to integer
