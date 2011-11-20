45762f4f2454dfb97aff26da1931b18aa4929a1f
chhavi mittal <chhavi.mittal91@gmail.com>
2011-11-19 21:57:46 -0800

    added directory for API

--- settings.py
+++ settings.py
@@ -127,7 +127,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'transactions'
+    'transactions',
+     'api'
 )
 
 # A sample logging configuration. The only tangible logging
