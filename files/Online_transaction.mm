d7222c3d91c26ab523d0d5688c0ed358e16058a0
Prateek Garg <prateekgargmat@gmail.com>
2011-11-22 16:27:33 +0530

    All files added(Class diag,uMl,test cases)

---
+++ files/Online_transaction.mm
@@ -0,0 +1,149 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1316262264841" ID="_ID_722548001" MODIFIED="1316267013816" STYLE="bubble" TEXT="online transaction">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#0033ff" CREATED="1316262264841" ID="_ID_1755024358" MODIFIED="1316267038772" POSITION="right" TEXT="money transfer">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<cloud/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#00b439" CREATED="1316262264841" ID="_ID_533979635" MODIFIED="1316266139387" TEXT="transfer across banks">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#990000" CREATED="1316262264841" ID="_ID_17258502" MODIFIED="1316266139385" TEXT="RTGS Transfer">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#990000" CREATED="1316262264841" ID="_ID_305688098" MODIFIED="1316266139384" TEXT="NEFT Transfer">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1316262264841" ID="_ID_1364292397" MODIFIED="1316266139384" TEXT="transfer within bank (third party tansfer or funds transfer)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#990000" CREATED="1316262264842" FOLDED="true" ID="_ID_339466984" MODIFIED="1316266139383" TEXT="Funds Transfer">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1316263603752" ID="ID_1731471050" MODIFIED="1316266046930" TEXT="Transferring to other account of the same person"/>
+</node>
+<node COLOR="#990000" CREATED="1316263204868" FOLDED="true" ID="ID_1049312790" MODIFIED="1316266139382" TEXT="Third Party Transfer">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1316263542448" ID="ID_385055187" MODIFIED="1316266046932" TEXT="This includes Transfer to someone&apos;s account in the same bank"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1316262264842" ID="_ID_1591673585" MODIFIED="1316266616427" TEXT="remitting customer">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="female1"/>
+<node COLOR="#990000" CREATED="1316262264842" ID="_ID_786197414" MODIFIED="1316266139381" TEXT="Account Type and Amount">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#990000" CREATED="1316262264842" ID="_ID_1841991354" MODIFIED="1316266139379" TEXT="Extra Service Charge">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#990000" CREATED="1316262264842" ID="_ID_589495978" MODIFIED="1316266139379" TEXT="updating account">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#990000" CREATED="1316262264842" ID="_ID_1840381743" MODIFIED="1316266139378" TEXT="current status of request">
+<edge COLOR="#808080"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#990000" CREATED="1316263697408" FOLDED="true" ID="ID_125270732" MODIFIED="1316266624471" TEXT="Various Constraints">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1316262264842" ID="_ID_1597972924" MODIFIED="1316266139376" TEXT="sufficient balance or not">
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+<node COLOR="#111111" CREATED="1316262264842" ID="_ID_711973640" MODIFIED="1316266139361" TEXT="transaction limits should not be voilated">
+<font NAME="SansSerif" SIZE="10"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1316262264843" ID="_ID_1704382161" MODIFIED="1316266139361" TEXT="beneficiary customer">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="female2"/>
+<node COLOR="#990000" CREATED="1316262264843" ID="_ID_993324900" MODIFIED="1316266139360" TEXT="amount received or not within specified time duration">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#990000" CREATED="1316262264843" ID="_ID_544259551" MODIFIED="1316266139358" TEXT="Updating Account">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1316262264841" HGAP="16" ID="ID_546528976" MODIFIED="1316267057291" POSITION="right" TEXT="API Required" VSHIFT="46">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<cloud/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#00b439" CREATED="1316262264841" FOLDED="true" ID="ID_995579978" MODIFIED="1316269120309" TEXT="Accounts">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#990000" CREATED="1316262264841" ID="ID_625229267" MODIFIED="1316267338329" TEXT="Account Information">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1316262264841" FOLDED="true" ID="ID_614911835" MODIFIED="1316269119274" TEXT="Identity Management">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#990000" CREATED="1316263204868" FOLDED="true" ID="ID_549205529" MODIFIED="1316269109029" TEXT="Session variables for the person currently logged in">
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1316263542448" ID="ID_1581275093" MODIFIED="1316266046932" TEXT="This includes Transfer to someone&apos;s account in the same bank"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1316262264844" HGAP="38" ID="_ID_284475201" MODIFIED="1316266139358" POSITION="left" TEXT="payment of goods and services" VSHIFT="3">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<cloud/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#00b439" CREATED="1316262264844" FOLDED="true" ID="_ID_862454843" MODIFIED="1316267255908" TEXT="Validating a request">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#990000" CREATED="1316264554608" ID="ID_627460092" MODIFIED="1316266046938" TEXT="checking whether payment can be done with specific bank&apos;s account">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1316264805981" ID="ID_257323409" MODIFIED="1316266046938" TEXT="account info and bank name in which money is transferred">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1316262264844" FOLDED="true" ID="_ID_1136626448" MODIFIED="1316267255026" TEXT="Redirection">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#990000" CREATED="1316264721938" ID="ID_501856734" MODIFIED="1316266046939" TEXT="After the transaction is over the customer is redirected to the original website with the required information about the transaction ">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1316262264845" FOLDED="true" ID="_ID_792695084" MODIFIED="1316267249725" TEXT="Security">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#990000" CREATED="1316264805981" ID="ID_1674070400" MODIFIED="1316267189839" TEXT="Validation Through SMS">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1316264805981" ID="ID_684559296" MODIFIED="1316267248011" TEXT="Proper Encryption while transfering Information">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1316264879817" FOLDED="true" ID="ID_918417479" MODIFIED="1316266223182" TEXT="Other Information">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#990000" CREATED="1316264908393" ID="ID_664995712" MODIFIED="1316266139352" TEXT="Service Charge of Bank">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#990000" CREATED="1316262264844" FOLDED="true" ID="ID_38069059" MODIFIED="1316266139349" TEXT="Type Of Transfer">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1316264588147" ID="ID_1005924752" MODIFIED="1316266046942" TEXT="checking whether it is interbank transfer or transfer across banks"/>
+</node>
+<node COLOR="#990000" CREATED="1316265018638" ID="ID_1029781879" MODIFIED="1316266139331">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Beneficiary Account Information
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+</map>
